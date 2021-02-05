710876b24411d2019d7ecf0ab396a43ac7e9f6ce
Chris Follen <christopher.follen@gmail.com>
2021-02-04 17:09:06 -0700

    commit tree 19937f1d9b89cfcc1e5e54a96933116a77271a33

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+
+    attr_accessor :roster 
+
+    def initialize name
+        @name = name
+        @roster = {}
+    end
+
+    def add_student student_name, grade
+        if roster[grade] 
+            roster[grade]<< student_name
+        else
+            roster[grade] = []
+            roster[grade]<< student_name
+        end
+    end
+
+    def grade grade_in
+        roster[grade_in]
+    end
+
+    def sort
+        roster.each {|grade, students| roster[grade] = students.sort}
+    end
+
+
+
+end
+ 
